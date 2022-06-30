0a1b657fce4df20d9e028a213fb9a45ba196255c
Ujusophy <sophyjelly718@gmail.com>
2022-06-30 12:17:18 -0700

    gfg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * 
- * Return: Always 0 (success)
+ * main - entry point
+ *
+ * Return 0 after printing the function
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
-	return(0);
+	puts("\programming is like buiding a multilingual puzzle");
+	return (0);
 }
