96c714e979af38556f539f4163900070d1c48dd5
Ujusophy <sophyjelly718@gmail.com>
2022-06-30 22:30:08 -0700

    th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+ 
 /**
- * main - entry point
- *
- * Return 0 after printing the function
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+ 
 int main(void)
 {
-	puts("\programming is like buiding a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	 
 	return (0);
 }
+
