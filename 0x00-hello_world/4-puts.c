7efb26ad37c211c333bd8942502c057c8f10c5ed
Ujusophy <sophyjelly718@gmail.com>
2022-06-30 23:06:51 -0700

    gh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  *  main - Entry point
- *   *
- *    *  Return: 0 number
- *     */
+ * main - Entry point
+ *
+ * Return: 0 number
+ */
 int main(void)
 {
-	                puts("\"Programming is like building a multilingual puzzle");
-			                        return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0)
 }
