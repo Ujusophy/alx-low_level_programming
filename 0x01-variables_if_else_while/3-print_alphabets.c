e7f42b2e53840ef2aa7b713d3efa1b27d6f3af24
Ujusophy <106748148+Ujusophy@users.noreply.github.com>
2022-07-03 16:45:25 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 /**
-  * main - Prints combination of numbers
-  *
-  * Return: Always (Success)
-  */
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-	int c, i;
-for (c = '0'; c <= '9'; c++)
-	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			if (c < i)
-			{
-				putchar(c);
-				putchar(i);
-if (c != '8' || (c == '8' && i != '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
+	char c;
+for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+        char b;
+for (b = 'A'; c <= 'Z'; c++)
+		putchar(b);
 putchar('\n');
-return (0);
+	return (0);
 }
