1aefdb412d09313ad02671d8eab9c0305db1adb9
Ujusophy <sophyjelly718@gmail.com>
2022-07-05 04:32:37 -0700

    sxhhc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include	<stdio.h>
+#include	<main.h>
 /**
- * main	-	Entry	point
+ * main	-	print	_putchar
  *
- * Return:	Always	0	(success)
+ * Return:	Always	0(success)
  */
 int	main(void)
 {
-	printf("_putchar\n");
+	printf("_putchar	\n");
 	return	(0);
 }
