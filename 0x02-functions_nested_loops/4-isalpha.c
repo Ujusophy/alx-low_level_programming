80d531be9c76af8ae00810532af3496a669e1594
Ujusophy <sophyjelly718@gmail.com>
2022-07-06 01:09:19 -0700

    seat

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/**
+ * File:	int _isalpha
+ *
+ *Auth	Ujusophy
+ */
+#include	"main.h"
+
+/**
+ * int _isalpha	-	check	for	alphabetic	character
+ * Return	1	if	c	is	a	letter
+ *
+ * Return	0	if	otherwise
+ */
+int _isalpha(int c)
+{
+	if	(c	>=	65	&&	c	<=	122)
+		return	(1);
+	else
+		return	(0);
+}
