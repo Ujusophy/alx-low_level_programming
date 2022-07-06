45c27cd0aa0c9fcd6d71cfc124ca1a85c35d1897
Ujusophy <106748148+Ujusophy@users.noreply.github.com>
2022-07-06 11:55:00 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+/**
+* File: 8-24_hours.c
+*
+*Auth Ujusophy
+*/
+
+#include "main.h"
+/**
+* void jack_bauer - prints every minute of jack Bauer day, starting from 0:00 to 23:59
+*/
+void jack_bauer(void)
+{
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
+_putchar('hour');
+{
+for (minute = 0; minute <= 59; minute++)
+_putchar('minute');
+}
+}
