100587a8fd694d8dc2154ef8062c3a9305fea34f
Ujusophy <106748148+Ujusophy@users.noreply.github.com>
2022-07-06 13:19:47 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,32 +1,31 @@
-/**
- * File:	5-sign.c
- *
- * Auth	Ujusophy
- */
-#include	"main.h"
+/*
+*File: 5-sign.c
+*Auth: Ujusophy
+*/
 
+#include "main.h"
 /**
- * int print_sign - print sign of a number
- * Return 1 and print + if n is greater than zero
- * Return 0 and print 0 if n is zero
- * Return -1 and print - if n is less than zero
-*
-*@n: the character to be checked
- */
+*print_sign -prints the sign of a number.
+*@n:The number of which the sign will be printed.
+*Return:1 and prints + if n is greater than zero.
+*	0 and prints 0 if n is zero.
+*	-1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
-
-if (n	>	0)
-_putchar('+');
-		return (1);
+	if (n > 0)
 	{
-if (n	== 0)
-_putchar('0');
-			return (0);
-}
-		{
-if (n	<	0)
-_putchar('-');
-				return (-1);
-		}
+		_putchar('+');
+		return (1);
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
