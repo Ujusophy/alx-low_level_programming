b8f809e67c86e782f40a5681bf9bd76d2bffbcc9
Ujusophy <106748148+Ujusophy@users.noreply.github.com>
2022-07-06 10:19:35 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,18 +16,15 @@ int print_sign(int n)
 
 if (n	>	0)
 _putchar('+');
-_putchar('\n');
 		return (1);
 	{
 if (n	== 0)
 _putchar('0');
-_putchar('\n');
 			return (0);
 }
 		{
 if (n	<	0)
 _putchar('-');
-_putchar('\n');
 				return (-1);
 		}
 	}
