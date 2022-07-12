6cf63c45d912c9ef5d374b61ad659579b07473a4
Ujusophy <sophyjelly718@gmail.com>
2022-07-12 13:06:27 -0700

    good

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include	"main.h"
+
+/**
+ * void reset_to_98	-	updates	the	value	of	n	to	98
+ * @n:	integer	pointer	to	a	variables	to	be	updated
+ *
+ * Return:	nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n	=	98;
+}
