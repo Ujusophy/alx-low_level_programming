22dad135d7e04556c5401e535bf4abac0441840b
Ujusophy <sophyjelly718@gmail.com>
2022-07-18 22:03:45 -0700

    jhg

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include	"main.h"
+
+/**
+ * _memset	-	function
+ *
+ * @n:	byte	of	the	memoryarea
+ *
+ * @s:	pointer
+ *
+ * @b:	variable
+ *
+ * Return:	to	the	pointer	@s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned	int	=	i
+		i	=	0
+		for(i	<	n	,	i++)
+			s[i]	=	b
+				return	(s);
